ad85b06a8e58bc4139403780de874536a828886b
Alexey Berezutsky <alexey.berezutsky@sidenis.com>
2015-12-11 01:04:17 +0300

    Initializing Watch Values

---
+++ karma.config.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ['browserify', 'jasmine'],
+        files: [
+            'src/**/*.js',
+            'tests/**/*_spec.js'
+        ],
+        preprocessors: {
+            'tests/**/*.js': ['jshint', 'browserify'],
+            'src/**/*.js': ['jshint', 'browserify']
+        },
+        browsers: ['PhantomJS'],
+        browserify: {
+            debug: true
+        }
+    })
+};
